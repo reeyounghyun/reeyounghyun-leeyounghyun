d90b6442d6e7d93aab6e87ee217d86182628dfb6
reeyounghyun <naem77716@gmailcom>
2024-07-31 14:18:20 +0900

    git add images

--- src/componentes/project.jsx
+++ src/componentes/project.jsx
@@ -1,8 +1,59 @@
-function project(){
-	return(
-		<div className="porject">
-		
-		</div>
-	)
+const porItems = [
+  {
+    id: 1,
+    title: '소리아월렛',
+    content: '일본 가상화페 지갑 기능 웹&앱 페이지'
+  },
+  {
+    id: 2,
+    title: "펀데이 코리아",
+    imgUrl:"https://raw.githubusercontent.com/reeyounghyun/leeyounghyun/main/public/logo192.png/",
+    content:"외국인을 대상으로 패키지 예약 & 여행리뷰, 체험단 모집 반응형 웹 페이지"
+  },
+  {
+    id: 3,
+    title: "자람 콘텐츠",
+    imgUrl:"https://jaramc.com/",
+    content: '체험 학습 예약, 활동 검색, 맞춤형 교육 프로그램 제공'
+  },
+  {
+    id: 4,
+    title: "소리빛",
+    imgUrl:"https://soribit.limefriends.com/",
+    content: "청각 및 언어 발달에 관련된 프로그램과 서비스를 제공하는 반응형 웹 페이지"
+  },
+  {
+    id: 5,
+    title: "박피디와 황배우",
+    imgUrl:"https://cancertainment.com/",
+    content: "암 환자 및 암을 극복한 사람들의 삶의 질 향상을 위한 다양한 사회 서비스를 제공, 반응형 웹 페이지"
+  },
+  {
+    id: 6,
+    title:"메디프",
+    imgUrl:"https://winwin.medif.or.kr/",
+    content:"병원, 전문가, 연구자 등의 자유롭게 네트워크할 수 있는 반응형 웹 페이지"
+  },
+  {
+    id: 7,
+    title: "에너지드림센터",
+    imgUrl:"https://seouledc.or.kr/",
+    content: "지속 가능한 도시 생활을 체험하고 배우는 교육 전시관 및 소개 반응형 웹 페이지"
+  },
+  {
+    id: 8,
+    title:"롯데",
+    content:"롯데 계열사 반응형 웹페이지"
+  },
+];
+
+function Project() {
+
+  return (
+    <div>
+      <h1>프로젝트 목록</h1>      
+    </div>
+  );
 }
-export default project;
+
+export default Project;
