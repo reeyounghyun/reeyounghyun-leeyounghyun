5e912fc4e449bfae814cc2eb486b63fc3f452f94
Young Hyun <naem77716@gmail.com>
2024-09-10 18:28:16 +0900

    주석

--- src/componentes/section1.jsx
+++ src/componentes/section1.jsx
@@ -10,7 +10,7 @@ const Section1 = () => {
   const sectionRef = useRef(null);
   const textRef = useRef(null);
 
-  // 커스텀 훅 호출
+  //Hooks 호출
   TextEffect1(sectionRef, textRef);
   
   return (
@@ -19,7 +19,7 @@ const Section1 = () => {
       ref={sectionRef}
       className="relative flex items-center justify-center w-full h-screen bg-[#0B1A2E]"
     >
-      <span className="absolute bottom-5 right-5 text-[5vw] leading-none">01.세로</span>
+      {/* <span className="absolute bottom-5 right-5 text-[5vw] leading-none">01.세로</span> */}
       <div ref={textRef} className="text-center text-[5vw] font-bold uppercase"> Welcome to my <br/> projects </div>
     </section>
   );
