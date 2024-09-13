79f53063ad1d3774da6ac1c9679b8d35b8560aab
Young Hyun <naem77716@gmail.com>
2024-09-13 18:23:07 +0900

    프로젝트 수정완료

--- src/componentes/porject.jsx
+++ src/componentes/porject.jsx
@@ -36,52 +36,11 @@ const Porject = () => {
     
   return (
     <section className='w-full pb-[100vh] relative mx-auto my-0 bg-black overflow-hidden'>
-        <div ref={el => sectionsRef.current[0] = el} className='w-full h-[110vh]'>
-            <div className="bg-[#0a3651] text-white rounded-t-[50px] shadow-[0px_0px_30px_rgba(0,0,0,0.3)] overflow-hidden">
-                <ul className="flex justify-between lg:flex-row flex-wrap">
-                    <li className="w-1/2">
-                        <a href="/">
-                            <p className="w-full">
-                                <img src="pc/porject1.png" alt="" className="w-full" />
-                            </p>
-                        </a>
-                    </li>
-                    <li className="w-1/2 pt-[150px] pl-[100px] box-border">
-                        <div className="mb-[30px]">
-                            <h2 className='text-[100px] leading-[0.9] '>자람콘텐츠</h2>
-                            <div className='w-[60%]'>
-                                <p className="text-[19px] pt-[3rem] mb-[30px] break-keep text-[#e9e9e9]">
-                                    자람 콘텐츠는 체험학습 예약, 교육 프로그램 제공 및 온라인 학습에 필요한 자료 제공하는 웹 사이트입니다. <br/>
-                                    <strong className='text-white font-bold'>JSX와 SCSS를</strong> 이용하여 Component 개발하였으며 <strong className='text-white font-bold'>Media Query를</strong> 사용하여 반응형 구현했습니다.
-                                </p>
-                            </div>
-                        </div>
-                        <div className="w-[60%] text-[14px] leading-[1.5]">
-                            <span className="block">
-                                <strong className='text-[16px] pb-2'>[참여 인원]</strong><br/> 
-                                기획1명 디자인 1명 프론트1명 백앤드 1명</span>
-                            <p className="mb-[30px] pt-8 tracking-[-1px] break-keep">
-                                <strong className='text-[16px] pb-2'>[작업 기여도]</strong><br/>
-                                체험 교육 콘텐츠 제공 웹페이지 구축&유지 보수 <br/> 
-                                (100% 사용자UI 구현했습니다)
-                            </p>
-                            <ul className='flex flex-wrap gap-3 text-sm text-[#ccc]'>
-                                <li>#React</li>
-                                <li>#반응형</li>
-                                <li>#협업</li>
-                                <li>#구축&유지 보수</li>
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-
         <div ref={el => sectionsRef.current[1] = el} className='w-full h-[110vh]'>
             <div className="bg-[#5f5c53] text-white rounded-t-[50px] shadow-[0px_0px_30px_rgba(0,0,0,0.3)] overflow-hidden">
                 <ul className="flex justify-between">
                     <li className="w-1/2">
-                        <a href="/">
+                        <a href="https://fundaykorea.com/" alt="펀데이코리아" target='blank'>
                             <p className="w-full">
                                 <img src="pc/porject2.png" alt="" className="w-full" />
                             </p>
@@ -118,37 +77,82 @@ const Porject = () => {
                 </ul>
             </div>
         </div>
+        <div ref={el => sectionsRef.current[0] = el} className='w-full h-[110vh]'>
+            <div className="bg-[#0a3651] text-white rounded-t-[50px] shadow-[0px_0px_30px_rgba(0,0,0,0.3)] overflow-hidden">
+                <ul className="flex justify-between lg:flex-row flex-wrap">
+                    <li className="w-1/2">
+                        <a href="https://jaramc.com/" alt="자람콘텐츠" target='blank'>
+                            <p className="w-full">
+                                <img src="pc/porject1.png" alt="" className="w-full" />
+                            </p>
+                        </a>
+                    </li>
+                    <li className="w-1/2 pt-[150px] pl-[100px] box-border">
+                        <div className="mb-[30px]">
+                            <h2 className='text-[100px] leading-[0.9] '>자람콘텐츠</h2>
+                            <div className='w-[60%]'>
+                                <p className="text-[19px] pt-[3rem] mb-[30px] break-keep text-[#e9e9e9]">
+                                    자람 콘텐츠는 체험학습 예약, 교육 프로그램 제공 및 온라인 학습에 필요한 자료 제공하는 웹 사이트입니다. <br/>
+                                    <strong className='text-white font-bold'>JSX와 SCSS를</strong> 이용하여 Component 개발하였으며 <strong className='text-white font-bold'>Media Query를</strong> 사용하여 반응형 구현했습니다.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="w-[60%] text-[14px] leading-[1.5]">
+                            <span className="block">
+                                <strong className='text-[16px] pb-2'>[참여 인원]</strong><br/> 
+                                기획1명 디자인 1명 프론트1명 백앤드 1명</span>
+                            <p className="mb-[30px] pt-8 tracking-[-1px] break-keep">
+                                <strong className='text-[16px] pb-2'>[작업 기여도]</strong><br/>
+                                체험 교육 콘텐츠 제공 웹페이지 구축&유지 보수 <br/> 
+                                (100% 사용자UI 구현했습니다)
+                            </p>
+                            <ul className='flex flex-wrap gap-3 text-sm text-[#ccc]'>
+                                <li>#React</li>
+                                <li>#반응형</li>
+                                <li>#협업</li>
+                                <li>#구축&유지 보수</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
         <div ref={el => sectionsRef.current[2] = el} className='w-full h-[110vh]'>
             <div className="bg-[#222] text-white rounded-t-[50px] shadow-[0px_0px_30px_rgba(0,0,0,0.3)] overflow-hidden">
                 <ul className="flex justify-between">
                     <li className="w-1/2">
-                        <a href="/">
+                        <a href="https://soribit.limefriends.com/"  alt="소리빛" target='blank'>
                             <p className="w-full">
                                 <img src="pc/project3.png" alt="" className="w-full" />
                             </p>
                         </a>
                     </li>
                     <li className="w-1/2 pt-[150px] pl-[100px] box-border">
-                        <div className="text-[120px] leading-[0.9] mb-[85px]">
-                            Design <span className="block">Agency</span>
+                        <div className="mb-[30px]">
+                            <h2 className='text-[100px] leading-[0.9] '>소리빛</h2>
+                            <div className='w-[60%]'>
+                                <p className="text-[19px] pt-[3rem] mb-[30px] break-keep text-[#e9e9e9]">
+                                    자람 콘텐츠는 체험학습 예약, 교육 프로그램 제공 및 온라인 학습에 필요한 자료 제공하는 웹 사이트입니다. <br/>
+                                    <strong className='text-white font-bold'>JSX와 SCSS를</strong> 이용하여 Component 개발하였으며 <strong className='text-white font-bold'>Media Query를</strong> 사용하여 반응형 구현했습니다.
+                                </p>
+                            </div>
                         </div>
-                        <div className="w-[60%] leading-[1.5]">
-                            <p className="text-[26px] leading-[1.2] mb-[30px]">
-                                In order to improve understanding of the layout, I personally cloned website, which uses a lot of animation and various layout.
+                        <div className="w-[60%] text-[14px] leading-[1.5]">
+                            <span className="block">
+                                <strong className='text-[16px] pb-2'>[참여 인원]</strong><br/> 
+                                기획1명 디자인 1명 프론트1명 백앤드 1명</span>
+                            <p className="mb-[30px] pt-8 tracking-[-1px] break-keep">
+                                <strong className='text-[16px] pb-2'>[작업 기여도]</strong><br/>
+                                체험 교육 콘텐츠 제공 웹페이지 구축&유지 보수 <br/> 
+                                (100% 사용자UI 구현했습니다)
                             </p>
-                            <p className="text-[15px] mb-[20px] tracking-[-1px]">
-                                ox구조와 전체적 레이아웃 제작에 대한 이해 및 실력향상을 위해 국내 기업사이트 중 2023년 트렌드에 맞고 인터렉션 구현이 화려하며 비쥬얼적으로 탁월한 웹사이트를 선정하여 Clone Coding으로 퍼블리싱 실력을 키우기위해 노력하였습니다.
-                            </p>
-                            <ul>
-                                <li className="relative pl-[20px] mb-[10px] before:content-[''] before:w-[5px] before:h-[5px] before:rounded-full before:bg-[rgba(0,0,0,0.5)] before:absolute before:left-0 before:top-1/2 before:transform before:-translate-y-1/2">
-                                    Html5 CSS
-                                </li>
-                                <li className="relative pl-[20px] mb-[10px] before:content-[''] before:w-[5px] before:h-[5px] before:rounded-full before:bg-[rgba(0,0,0,0.5)] before:absolute before:left-0 before:top-1/2 before:transform before:-translate-y-1/2">
-                                    jQuery
-                                </li>
-                                <li className="relative pl-[20px] mb-[10px] before:content-[''] before:w-[5px] before:h-[5px] before:rounded-full before:bg-[rgba(0,0,0,0.5)] before:absolute before:left-0 before:top-1/2 before:transform before:-translate-y-1/2">
-                                    Javascript
-                                </li>
+                            <ul className='flex flex-wrap gap-3 text-sm text-[#ccc]'>
+                                <li>#React</li>
+                                <li>#반응형</li>
+                                <li>#협업</li>
+                                <li>#구축&유지 보수</li>
                             </ul>
                         </div>
                     </li>
